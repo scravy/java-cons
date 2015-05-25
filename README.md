0a1a779ae6b4f09f8cdd9ac8416940ceb6e0206b
Julian Fleischer <julian.fleischer@fu-berlin.de>
2015-05-25 12:02:21 +0200

    implementation for 1.1.0 (lacking tests)

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 java-cons
 =========
 
+Changelog
+---------
+
+### 1.1.0
++ `Cons` implements the `List` interface now.
++ `drop` and `take` added.
--- src/main/java/de/scravy/cons/Cons.java
+++ src/main/java/de/scravy/cons/Cons.java
@@ -2,14 +2,21 @@ package de.scravy.cons;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Deque;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
 import java.util.NoSuchElementException;
+import java.util.Objects;
+import java.util.Set;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.Value;
 import de.scravy.pair.Pair;
+import de.scravy.pair.Pairs;
 
 /**
  * A singly linked, persistent (immutable), homogeneous list.
@@ -25,7 +32,7 @@ import de.scravy.pair.Pair;
  */
 @Value
 @AllArgsConstructor(access = AccessLevel.PRIVATE)
-public class Cons<E> implements Pair<E, Cons<E>>, Iterable<E> {
+public class Cons<E> implements Pair<E, Cons<E>>, Iterable<E>, List<E> {
 
   private static Cons<?> EMPTY = new Cons<Object>(null, null);
 
@@ -121,6 +128,7 @@ public class Cons<E> implements Pair<E, Cons<E>>, Iterable<E> {
    * @since 1.0.0
    * @return Whether this list is empty or not.
    */
+  @Override
   public boolean isEmpty() {
     return this == EMPTY;
   }
@@ -184,6 +192,7 @@ public class Cons<E> implements Pair<E, Cons<E>>, Iterable<E> {
    *           Thrown if the list does not contain this many elements (iff
    *           <code>getLength() ≥ index</code>).
    */
+  @Override
   public E get(final int ix) {
     Cons<E> current = this;
     int i = 0;
@@ -230,5 +239,224 @@ public class Cons<E> implements Pair<E, Cons<E>>, Iterable<E> {
         throw new UnsupportedOperationException();
       }
     };
+  }
+
+  @Override
+  public int size() {
+    return getLength();
+  }
+
+  @Override
+  public boolean contains(final Object o) {
+    for (final E e : this) {
+      if (e == o || (e != null && e.equals(o))) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  public boolean doesNotContain(final Object o) {
+    for (final E e : this) {
+      if (e == o || (e != null && e.equals(o))) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public Object[] toArray() {
+    return toArray(new Object[getLength()]);
+  }
+
+  @Override
+  public <T> T[] toArray(final T[] a) {
+    return toArray(a, 0);
+  }
+
+  @SuppressWarnings("unchecked")
+  public <T> T[] toArray(final T[] a, final int offset) {
+    final int i = offset;
+    for (final E e : this) {
+      if (i + offset >= a.length) {
+        return a;
+      }
+      a[i] = (T) e;
+    }
+    return a;
+  }
+
+  @Override
+  public boolean add(final E e) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean remove(final Object o) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean containsAll(final Collection<?> c) {
+    final Set<E> set = new HashSet<E>(this);
+    for (final Object e : c) {
+      if (set.contains(e)) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  @Override
+  public boolean addAll(final Collection<? extends E> c) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean addAll(final int index, final Collection<? extends E> c) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean removeAll(final Collection<?> c) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public boolean retainAll(final Collection<?> c) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void clear() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public E set(final int index, final E element) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public void add(final int index, final E element) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public E remove(final int index) {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public int indexOf(final Object o) {
+    int i = 0;
+    for (final E e : this) {
+      if (Objects.equals(e, o)) {
+        return i;
+      }
+      i += 1;
+    }
+    return -1;
+  }
+
+  @Override
+  public int lastIndexOf(final Object o) {
+    int i = 0;
+    int ix = -1;
+    for (final E e : this) {
+      if (Objects.equals(e, o)) {
+        ix = i;
+      }
+      i += 1;
+    }
+    return ix;
+  }
+
+  @Override
+  public ListIterator<E> listIterator() {
+    throw new UnsupportedOperationException();
+  }
+
+  @Override
+  public ListIterator<E> listIterator(final int index) {
+    throw new UnsupportedOperationException();
+  }
+
+  public Cons<E> take(final int n) {
+    final Deque<E> stack = new LinkedList<>();
+    int i = 0;
+    for (final E e : this) {
+      if (i >= n) {
+        break;
+      }
+      stack.push(e);
+      i += 1;
+    }
+    Cons<E> result = Cons.empty();
+    while (!stack.isEmpty()) {
+      result = result.cons(stack.pop());
+    }
+    return result;
+  }
+
+  public Cons<E> drop(final int n) {
+    return subList(n);
+  }
+
+  public Cons<E> subList(final int ix) {
+    Cons<E> current = this;
+    int i = 0;
+    while (i < ix && current != EMPTY) {
+      current = current.tail;
+      i += 1;
+    }
+    return current;
+  }
+
+  public Cons<E> reverse() {
+    final Deque<E> stack = new LinkedList<>();
+    for (final E e : this) {
+      stack.push(e);
+    }
+    Cons<E> current = Cons.empty();
+    while (!stack.isEmpty()) {
+      current = current.cons(stack.pop());
+    }
+    return current;
+  }
+
+  @Override
+  public Cons<E> subList(final int fromIndex, final int toIndex) {
+    return drop(fromIndex).take(toIndex - fromIndex);
+  }
+
+  @Override
+  public int hashCode() {
+    return Pairs.hashCode(this);
+  }
+
+  @Override
+  public boolean equals(final Object other) {
+    if (this == other) {
+      return true;
+    }
+    if (other instanceof Pair) {
+      return Pairs.equals(this, other);
+    }
+    if (other == null) {
+      return false;
+    }
+    if (getClass() != other.getClass()) {
+      return false;
+    }
+    final Iterator<?> it1 = iterator();
+    final Iterator<?> it2 = ((Cons<?>) other).iterator();
+    while (it1.hasNext() && it2.hasNext()) {
+      if (!Objects.equals(it1.next(), it2.next())) {
+        return false;
+      }
+    }
+    return !it1.hasNext() && !it2.hasNext();
   }
 }
